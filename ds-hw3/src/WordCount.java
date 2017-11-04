fb25a7aaebb0c0d8ca13c305f60e02b471aedb99
jtautry <jtautry@gmail.com>
2017-11-03 23:57:09 -0500

    aggregating by chapter

--- ds-hw3/src/WordCount.java
+++ ds-hw3/src/WordCount.java
@@ -5,47 +5,95 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
 
     public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable>{
+            extends Mapper<Object, Text, Text, Text>{
 
-        private final static IntWritable one = new IntWritable(1);
+        //private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            //toLower and replace anything besides a-z with a space
-            String newValue = value.toString().toLowerCase().replaceAll("^[a-z]+$", " ");
+            InputSplit inputSplit = context.getInputSplit();
+            String fileName = ((FileSplit) inputSplit).getPath().getName();
 
-            StringTokenizer itr = new StringTokenizer(value.toString());
+            //toLower and replace anything besides a-z with a space
+            String newValue = value.toString().toLowerCase().replaceAll("[^a-z]", " ");
+
+            StringTokenizer itr = new StringTokenizer(newValue);
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
-                context.write(word, one);
+                //context.write(word, one);
+                context.write(word, new Text(fileName));
             }
         }
     }
 
     public static class IntSumReducer
-            extends Reducer<Text,IntWritable,Text,IntWritable> {
-        private IntWritable result = new IntWritable();
+            extends Reducer<Text,Text,Text,Text> {
+        //private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values,
+        public void reduce(Text key, Iterable<Text> values,
                            Context context
         ) throws IOException, InterruptedException {
             int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
-            result.set(sum);
-            context.write(key, result);
+            boolean check = false;
+
+            String fileName="";
+
+            String textString="{";
+
+            /*reducing by counting the filename against each key*/
+
+            for (Text val : values)
+
+                {
+
+                    if(!check)
+
+                    {
+
+                        fileName=val.toString();
+
+                        check=true;
+
+                    }
+
+                    if (fileName.equals(val.toString())){
+
+                        sum=sum+1; //for counting the number of occurance in each file
+
+                    }
+
+                    else
+
+                    {
+
+                        textString+=fileName + "="+sum +", ";
+
+                        fileName=val.toString();
+
+                        sum=1;
+
+                    }
+
+                }
+
+                textString+= fileName + "="+sum +"} \n"; //making output pattern
+
+            context.write(key, new Text(textString));
+
         }
+
     }
 
     public static void main(String[] args) throws Exception {
@@ -56,7 +104,7 @@ public class WordCount {
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
